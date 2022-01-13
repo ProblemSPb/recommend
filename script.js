cb71b447e0374d6c835012f9c06fbf444b512171
ProblemSPb <ussr83@mail.ru>
2022-01-13 22:36:10 +0100

    Result page is shown only after all API requests are returned

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let inputTag = document.getElementById("input1");
 
-inputTag.addEventListener("keyup", function(event) {
-    if(event.key === "Enter") {
+inputTag.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
         validateForm();
     }
 });
 
+// validate form; if empty -> show warning popup, if not empty -> process user's input
 function validateForm() {
     let input = document.getElementById("input1").value;
     let goal;
@@ -17,20 +18,14 @@ function validateForm() {
             title: "Something is missing",
             message: "Set at least one goal for your personal development",
             buttonText: "Okay"
-          })
+        })
     } else {
         goal = input;
 
-        callGoodreadsAPI(goal);
-        callGoogleSearchAPI(goal);
+        // shows Load Icon
+        document.querySelector(".preload").style.display = "initial";
 
-        document.getElementById("submit1").innerHTML = "re-start";
-        document.getElementById("submit1").setAttribute( "onClick", "reloadPage();" );
-
-        removeElement("hint_text");
-        showlElement("google-search-container")
-        showGoodreads();
-
+        sendingAPIrequests(goal);
     }
 }
 
@@ -41,76 +36,73 @@ function reloadPage() {
 // hide element from the page
 function removeElement(arg) {
     let element = document.getElementById(arg);
-
     element.remove();
 }
 
 // show hidden elements
 function showlElement(arg) {
     let element = document.getElementById(arg);
-
     element.removeAttribute("hidden");
 }
 
 function showGoodreads() {
     let element = document.getElementById("goodread-container");
-    if(window.getComputedStyle(element).display === "none") {
+    if (window.getComputedStyle(element).display === "none") {
         element.style.display = "flex";
     };
 }
 
-// returns Goodreads books per user's input
-function callGoodreadsAPI(goal) {
+
+// Loader
+
+const loader = document.querySelector(".preload");
+const emoji = document.querySelector(".emoji");
+
+const emojis = ["🕐", "🕜", "🕑", "🕝", "🕒", "🕞", "🕓", "🕟", "🕔", "🕠", "🕕", "🕡", "🕖", "🕢", "🕗", "🕣", "🕘", "🕤", "🕙", "🕥", "🕚", "🕦", "🕛", "🕧"];
+
+const interval = 125;
+
+const loadEmojis = (arr) => {
+    setInterval(() => {
+        emoji.innerText = arr[Math.floor(Math.random() * arr.length)];
+    }, interval);
+}
+
+const init = () => {
+    loadEmojis(emojis);
+}
+init();
+// End of Loader
+
+
+
+const goodReads = (goal) => {
     // remove any extra spaces in the beginning and end of user input
     goal = goal.trim();
     // replace spaces with %20 for the URL
     goal = goal.replaceAll(' ', '%20');
 
-    let URL = "https://goodreads-books.p.rapidapi.com/search?q=public%20speech&page=1";
-    URL = "https://goodreads-books.p.rapidapi.com/search?q=" + goal + "&page=1";
+    // let URL = "https://goodreads-books.p.rapidapi.com/search?q=public%20speech&page=1";
+    let URL = "https://goodreads-books.p.rapidapi.com/search?q=" + goal + "&page=1";
 
-    fetch(URL, {
+    return fetch(URL, {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "goodreads-books.p.rapidapi.com",
             "x-rapidapi-key": "c60b70da79mshef98822fe0efea0p1abc63jsnda69cf942a1a"
         }
     })
-    .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
+};
 
-        document.getElementById("book1_link").href = data[0].url;
-        document.getElementById("book1_img").src = data[0].smallImageURL;
-        document.getElementById("book1_title").innerHTML = data[0].title + " by " + data[0].author;
-
-        document.getElementById("book2_link").href = data[1].url;
-        document.getElementById("book2_img").src = data[1].smallImageURL;
-        document.getElementById("book2_title").innerHTML = data[1].title + " by " + data[1].author;;
-
-        document.getElementById("book3_link").href = data[2].url;
-        document.getElementById("book3_img").src = data[2].smallImageURL;
-        document.getElementById("book3_title").innerHTML = data[2].title + " by " + data[2].author;;
-
-      })
-    .catch(err => {
-        console.error(err);
-    });
-}
-
-// returns Google Search results per user's input
-function callGoogleSearchAPI(goal) {
+const google = (goal) => {
     // remove any extra spaces in the beginning and end of user input
     goal = goal.trim();
     // replace spaces with + for the URL
     goal = goal.replaceAll(' ', '+');
 
-    let URL = "https://google-search3.p.rapidapi.com/api/v1/search/q=coding+tutorial&num=5";
-    URL = "https://google-search3.p.rapidapi.com/api/v1/search/q=" + goal + "+tutorial&num=5";
+    let URL = "https://google-search3.p.rapidapi.com/api/v1/search/q=" + goal + "+tutorial";
 
-    fetch(URL, {
+    return fetch(URL, {
         "method": "GET",
         "headers": {
             "x-user-agent": "desktop",
@@ -119,32 +111,67 @@ function callGoogleSearchAPI(goal) {
             "x-rapidapi-key": "c60b70da79mshef98822fe0efea0p1abc63jsnda69cf942a1a"
         }
     })
-    .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
+};
 
-        document.getElementById("search_result1_link").innerHTML = data.results[0].title;
-        document.getElementById("search_result1_descr").innerHTML = data.results[0].description;
-        document.getElementById("search_result1_link").href = data.results[0].link;
+function sendingAPIrequests(goal) {
 
-        document.getElementById("search_result2_link").innerHTML = data.results[1].title;
-        document.getElementById("search_result2_descr").innerHTML = data.results[1].description;
-        document.getElementById("search_result2_link").href = data.results[1].link;
+    // allSettled => to execute only when both requests are finished
+    Promise.allSettled([goodReads(goal), google(goal)]).
+        then(
+            ([goodReadsResponse, googleResponse]) => {
+                goodReadsResponse.value.json().then(json => {
+                    if (json) {
+                        console.log(json);
 
-        document.getElementById("search_result3_link").innerHTML = data.results[2].title;
-        document.getElementById("search_result3_descr").innerHTML = data.results[2].description;
-        document.getElementById("search_result3_link").href = data.results[3].link;
+                        document.getElementById("book1_link").href = json[0].url;
+                        document.getElementById("book1_img").src = json[0].smallImageURL;
+                        document.getElementById("book1_title").innerHTML = json[0].title + " by " + json[0].author;
 
-      })
-    .catch(err => {
-        console.error(err);
-    });
+                        document.getElementById("book2_link").href = json[1].url;
+                        document.getElementById("book2_img").src = json[1].smallImageURL;
+                        document.getElementById("book2_title").innerHTML = json[1].title + " by " + json[1].author;
+
+                        document.getElementById("book3_link").href = json[2].url;
+                        document.getElementById("book3_img").src = json[2].smallImageURL;
+                        document.getElementById("book3_title").innerHTML = json[2].title + " by " + json[2].author;
+                    }
+                });
+
+                googleResponse.value.json().then(json => {
+                    if (json) {
+                        console.log(json);
+
+                        document.getElementById("search_result1_link").innerHTML = json.results[0].title;
+                        document.getElementById("search_result1_descr").innerHTML = json.results[0].description;
+                        document.getElementById("search_result1_link").href = json.results[0].link;
+
+                        document.getElementById("search_result2_link").innerHTML = json.results[1].title;
+                        document.getElementById("search_result2_descr").innerHTML = json.results[1].description;
+                        document.getElementById("search_result2_link").href = json.results[1].link;
+
+                        document.getElementById("search_result3_link").innerHTML = json.results[2].title;
+                        document.getElementById("search_result3_descr").innerHTML = json.results[2].description;
+                        document.getElementById("search_result3_link").href = json.results[3].link;
+
+                    }
+                });
+
+                // hiding elements that are not needed and showing new elements with request results
+                document.getElementById("submit1").innerHTML = "re-start";
+                document.getElementById("submit1").setAttribute("onClick", "reloadPage();");
+                document.querySelector(".preload").style.display = "none";
+                removeElement("hint-text");
+                showlElement("google-search-container")
+                showGoodreads();
+
+            }
+        );
 }
+
+// ctrl + OPT + F
 
 
 //TODO: 
-// изменить дефолтовый алерт на что-то получше
-// make a "wait" icon
+// make a nice "wait" icon
+// refactoring, split js into separate files
 // выбрать самые новые или популярные книги на Goodreads
-// не показывать страницу с результатами, пока не вернутся результаты API calls
